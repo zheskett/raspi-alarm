28c38a465679eada5bda60beed12f1a61ca25f65
Zachary Heskett <zheskett@gmail.com>
2025-10-04 06:30:57 -0400

    Enhance alarm system with DHT11 sensor integration and OLED display updates

--- alarm/control.py
+++ alarm/control.py
@@ -1,4 +1,34 @@
-from gpiozero import Button
+import time
+
+import adafruit_dht
+import board
+import spidev
+from gpiozero import Button, DigitalOutputDevice, TonalBuzzer
+from PIL import Image, ImageDraw, ImageFont
+
+COMMAND_DICT: dict[str, int] = {
+    "display_on": 0xAF,  # Turn the display on
+    "display_off": 0xAE,  # Turn the display off
+    "set_contrast": 0x81,  # Set contrast control (1 byte follow)
+    "normal_display": 0xA6,  # Normal display (not inverted)
+    "addressing_mode": 0x20,  # Set memory addressing mode (1 byte follow)
+    "column_addr": 0x21,  # Set column address (2 bytes follow: start, end)
+    "page_addr": 0x22,  # Set page address (2 bytes follow: start, end)
+    "entire_display_on": 0xA5,  # Entire display ON (all pixels on)
+    "use_ram": 0xA4,  # Resume to RAM content display
+    "multiplex_ratio": 0xA8,  # Set multiplex ratio (1 byte follow: height-1)
+    "display_offset": 0xD3,  # Set display offset (1 byte follow)
+    "start_line_0": 0x40,  # Set display start line to 0
+    "charge_pump": 0x8D,  # Charge pump setting (1 byte follow)
+    "pre-charge_period": 0xD9,  # Set pre-charge period (1 byte follow)
+    "VCOMH_deselect_level": 0xDB,  # Set VCOMH deselect level (1 byte follow)
+    "segment_remap_mirror_x": 0xA1,  # Set segment remap (mirror X)
+    "segment_remap_normal": 0xA0,  # Set segment remap (normal)
+    "com_scan_dec": 0xC8,  # Set COM output scan direction (mirror Y)
+    "com_scan_inc": 0xC0,  # Set COM output scan direction (normal)
+    "com_pins": 0xDA,  # Set COM pins
+    "osc_freq": 0xD5,  # Set oscillator frequency (1 byte follow)
+}
 
 
 class AlarmButton:
@@ -25,3 +55,123 @@ class AlarmButton:
             self.just_pressed = False
 
         self.down = active
+
+
+class AlarmBuzzer:
+    """
+    Buzzer on a GPIO pin
+    """
+
+    def __init__(self, pin: int | str):
+        self.tonal_buzzer = TonalBuzzer(pin)
+        self.tonal_buzzer.stop()
+
+
+class DHT11Sensor:
+    """
+    DHT11 temperature and humidity sensor on a GPIO pin
+    """
+
+    def __init__(self):
+        self.sensor = adafruit_dht.DHT11(board.D19)
+
+    def read(self) -> tuple[float | None, float | None]:
+        return self.sensor.temperature, self.sensor.humidity
+
+
+class AlarmDisplay:
+    """
+    SSD1306 OLED screen on SPI
+    128x64
+    """
+
+    WIDTH: int = 128
+    HEIGHT: int = 64
+    PAGES: int = HEIGHT // 8
+
+    def __init__(self, dc_pin: int | str, rst_pin: int | str):
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 8_000_000
+        self.spi.mode = 0b00
+        self.dc = DigitalOutputDevice(dc_pin, initial_value=False)
+        self.rst = DigitalOutputDevice(rst_pin, active_high=False, initial_value=False)
+
+        self.reset()
+
+        # region: Initialize commands
+        self.exec_cmd(COMMAND_DICT["display_off"])
+        self.exec_cmd(COMMAND_DICT["multiplex_ratio"], bytes([0x3F]))
+        self.exec_cmd(COMMAND_DICT["display_offset"], bytes([0x00]))
+        self.exec_cmd(COMMAND_DICT["start_line_0"])
+        self.exec_cmd(COMMAND_DICT["segment_remap_normal"])
+        self.exec_cmd(COMMAND_DICT["com_scan_inc"])
+        self.exec_cmd(COMMAND_DICT["com_pins"], bytes([0x12]))
+        self.exec_cmd(COMMAND_DICT["use_ram"])
+        self.exec_cmd(COMMAND_DICT["normal_display"])
+        self.exec_cmd(COMMAND_DICT["osc_freq"], bytes([0x80]))
+        self.exec_cmd(COMMAND_DICT["charge_pump"], bytes([0x14]))
+        self.dim_level(0)
+        self.exec_cmd(COMMAND_DICT["addressing_mode"], bytes([0x00]))
+        self.exec_cmd(COMMAND_DICT["segment_remap_mirror_x"])
+        self.exec_cmd(COMMAND_DICT["com_scan_dec"])
+        self.exec_cmd(COMMAND_DICT["column_addr"], bytes([0x00, self.WIDTH - 1]))
+        self.exec_cmd(COMMAND_DICT["page_addr"], bytes([0x00, self.PAGES - 1]))
+        self.exec_cmd(COMMAND_DICT["display_on"])
+        # endregion
+
+    def exec_cmd(self, cmd: int, args: bytes = bytes()) -> None:
+        self.dc.off()
+        self.spi.writebytes2(bytes([cmd]) + args)
+
+    def exec_data(self, data: bytes) -> None:
+        self.dc.on()
+        self.spi.writebytes2(data)
+
+    def white_screen(self) -> None:
+        self.exec_data(bytes([0xFF] * (self.WIDTH * self.PAGES)))
+
+    def clear_screen(self) -> None:
+        self.exec_data(bytes([0x00] * (self.WIDTH * self.PAGES)))
+
+    def dim_level(self, level: int):
+        if level not in range(0, 1):
+            raise ValueError("Level must be 0 or 1")
+
+        if level == 0:
+            self.exec_cmd(COMMAND_DICT["set_contrast"], bytes([0x00]))
+            self.exec_cmd(COMMAND_DICT["pre-charge_period"], bytes([0x11]))
+            self.exec_cmd(COMMAND_DICT["VCOMH_deselect_level"], bytes([0x20]))
+        else:
+            self.exec_cmd(COMMAND_DICT["set_contrast"], bytes([0x7F]))
+            self.exec_cmd(COMMAND_DICT["pre-charge_period"], bytes([0xF1]))
+            self.exec_cmd(COMMAND_DICT["VCOMH_deselect_level"], bytes([0x20]))
+
+    def write_image(self, image: Image.Image) -> None:
+        if image.size != (self.WIDTH, self.HEIGHT):
+            image = image.resize((self.WIDTH, self.HEIGHT), Image.Resampling.NEAREST)
+
+        pixel_data = bytearray(list(image.convert("1").getdata(0)))
+        buffer = bytearray(self.WIDTH * self.PAGES)
+        for p in range(self.PAGES):
+            for x in range(self.WIDTH):
+                for page_col in range(8):
+                    pixel = pixel_data[x + (p * 8 + page_col) * self.WIDTH]
+                    buffer[x + p * self.WIDTH] |= pixel << page_col
+
+        self.exec_data(buffer)
+
+    def reset(self) -> None:
+        self.rst.off()
+        time.sleep(0.005)
+        self.rst.on()
+        time.sleep(0.005)
+        self.rst.off()
+        time.sleep(0.02)
+
+    def close(self) -> None:
+        self.exec_cmd(COMMAND_DICT["display_off"])
+        self.exec_cmd(COMMAND_DICT["charge_pump"], bytes(0x10))
+        self.spi.close()
+        self.rst.close()
+        self.dc.close()
--- alarm/loop.py
+++ alarm/loop.py
@@ -1,12 +1,15 @@
 import asyncio
 import curses
+import time
 from datetime import datetime
 
-from alarm.control import AlarmButton
+from alarm.control import *
 
 UPDATE_S = 0.2
 
 now = datetime.now()
+dht_sensor = DHT11Sensor()
+temp, hum = 0.0, 0.0
 
 
 def entry(stdscr: curses.window):
@@ -14,18 +17,42 @@ def entry(stdscr: curses.window):
 
 
 async def alarm_loop(stdscr: curses.window):
-    buttons: list[AlarmButton] = []
+    display = AlarmDisplay(25, 24)
+    display.clear_screen()
+
+    time_img = Image.new("1", (display.WIDTH, display.HEIGHT), 0)
+    img_draw = ImageDraw.Draw(time_img)
+    big_font = ImageFont.load_default(32)
+    small_font = ImageFont.load_default(12)
+
+    buttons: dict[str, AlarmButton] = {
+        "enable_alarm": AlarmButton(22),
+        "select": AlarmButton(27),
+        "right": AlarmButton(4),
+        "left": AlarmButton(21),
+    }
+
+    buzzer_r = AlarmBuzzer(26)
+    buzzer_l = AlarmBuzzer(17)
 
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     stdscr.nodelay(True)
     stdscr.addstr(f"Raspi Alarm\nStarted {now_str}\n\nPress Enter to exit...\n")
     update_task = asyncio.create_task(update_time())
+    dht_task = asyncio.create_task(update_dht())
 
     while True:
-        for b in buttons:
+        for b in buttons.values():
             b.update_press()
 
-        stdscr.addstr(5, 0, f"Time: {now.strftime('%H:%M:%S')}\n")
+        time_str = now.strftime("%H:%M:%S")
+
+        img_draw.rectangle((0, 0, display.WIDTH, display.HEIGHT), outline=0, fill=0)
+        img_draw.text((0, -8), time_str, fill=1, font=big_font)
+        img_draw.text((0, 50), f"T:{temp:.0f}°F H:{hum:.0f}%", fill=1, font=small_font)
+        display.write_image(time_img)
+
+        stdscr.addstr(5, 0, f"Time: {time_str}\n")
         try:
             key = stdscr.getkey()
             if key == "\n":
@@ -34,9 +61,21 @@ async def alarm_loop(stdscr: curses.window):
             pass
 
         # Required to allow other tasks to run
-        await asyncio.sleep(0)
+        await asyncio.sleep(0.05)
 
     update_task.cancel()
+    dht_task.cancel()
+    dht_sensor.sensor.exit()
+    display.close()
+
+
+async def update_dht():
+    global dht_sensor, temp, hum
+    while True:
+        t, h = dht_sensor.read()
+        if t is not None and h is not None:
+            temp, hum = c_to_f(t), h
+        await asyncio.sleep(10.0)
 
 
 async def update_time():
@@ -44,3 +83,7 @@ async def update_time():
     while True:
         now = datetime.now()
         await asyncio.sleep(UPDATE_S)
+
+
+def c_to_f(c: float) -> float:
+    return c * 9.0 / 5.0 + 32.0
